198de489c0b122a26789b7063f86ca105f57ace4
lyokato <lyo.kato@gmail.com>
2010-07-14 15:28:42 +0900

    added documents for Formatters

--- lib/OAuth/Lite2/Formatter/XML.pm
+++ lib/OAuth/Lite2/Formatter/XML.pm
@@ -44,4 +44,58 @@ sub parse {
     return $hash;
 }
 
+=head1 NAME
+
+OAuth::Lite2::Formatter::XML - OAuth 2.0 XML formatters store
+
+=head1 SYNOPSIS
+
+    my $formatter = OAuth::Lite2::Formatter::XML->new;
+    my $obj = $formatter->parse( $string );
+    $string = $formatter->format( $obj );
+
+=head1 DESCRIPTION
+
+DEPRECATED.
+OAuth 2.0 XML formatter.
+
+=head1 METHODS
+
+=head2 name
+
+Accessor for name of this format, "xml".
+
+=head2 type
+
+Accessor for content-type of this format, "application/xml".
+
+=head2 format( $object )
+
+    my $xml_string = $formatter->format( $obj );
+
+=head2 parse( $xml_string )
+
+    my $obj = $formatter->parse( $xml_string );
+
+=head1 SEE ALSO
+
+L<OAuth::Lite2::Formatter>
+L<OAuth::Lite2::Formatters>
+L<OAuth::Lite2::Formatter::JSON>
+L<OAuth::Lite2::Formatter::FormURLEncoded>
+
+=head1 AUTHOR
+
+Lyo Kato, E<lt>lyo.kato@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Lyo Kato
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
 1;
