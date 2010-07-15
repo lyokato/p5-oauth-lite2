9b3c5af42b2de22e3b7bdcbbfb794cd5e546c32d
lyokato <lyo.kato@gmail.com>
2010-07-15 12:43:27 +0900

    added model validation and document onto middleware

--- lib/Plack/Middleware/Auth/OAuth2/ProtectedResource.pm
+++ lib/Plack/Middleware/Auth/OAuth2/ProtectedResource.pm
@@ -8,6 +8,7 @@ use parent 'Plack::Middleware';
 use Plack::Request;
 use Plack::Util::Accessor qw(realm data_handler);
 use Try::Tiny;
+use Carp ();
 
 use OAuth::Lite2::Server::Error;
 use OAuth::Lite2::ParamMethods;
@@ -37,11 +38,19 @@ sub call {
         OAuth::Lite2::Server::Error::InvalidToken->throw
             unless $access_token;
 
+        Carp::croak "OAuth::Lite2::Server::DataHandler::get_access_token doesn't return OAuth::Lite2::Model::AccessToken"
+            unless $access_token->isa("OAuth::Lite2::Model::AccessToken");
+
         OAuth::Lite2::Server::Error::ExpiredToken->throw
             unless ($access_token->created_on + $access_token->expires_in > time());
 
         my $auth_info = $dh->get_auth_info_by_id($access_token->auth_id);
-        # TODO validate auth_info
+
+        OAuth::Lite2::Server::Error::InvalidToken->throw
+            unless $auth_info;
+
+        Carp::croak "OAuth::Lite2::Server::DataHandler::get_auth_info_by_id doesn't return OAuth::Lite2::Model::AuthInfo"
+            unless $auth_info->isa("OAuth::Lite2::Model::AuthInfo");
 
         $dh->validate_client_by_id($auth_info->client_id)
             or OAuth::Lite2::Server::Error::InvalidToken->throw;
@@ -51,7 +60,7 @@ sub call {
 
         $env->{REMOTE_USER}    = $auth_info->user_id;
         $env->{X_OAUTH_CLIENT} = $auth_info->client_id;
-        $env->{X_OAUTH_SCOPE}  = $auth_info->scope;
+        $env->{X_OAUTH_SCOPE}  = $auth_info->scope if $auth_info->scope;
 
         return;
 
@@ -84,5 +93,71 @@ sub call {
 
     return $error_res || $self->app->($env);
 }
+
+=head1 NAME
+
+Plack::Middleware::Auth::OAuth2::ProtectedResource - middleware for OAuth 2.0 Protected Resource endpoint
+
+=head1 SYNOPSIS
+
+    my $app = sub {...};
+    builder {
+        enable "Plack::Middleware::OAuth2::ProtectedResource",
+            data_handler => "YourApp::DataHandler";
+        enable "Plack::Middleware::JSONP";
+        enable "Plack::Middleware::ContentLength";
+        $app;
+    };
+
+    # and on your controller
+    $plack_request->env->{REMOTE_USER};
+    $plack_request->env->{X_OAUTH_CLIENT_ID};
+    $plack_request->env->{X_OAUTH_SCOPE};
+
+=head1 DESCRIPTION
+
+middleware for OAuth 2.0 Protected Resource endpoint
+
+=head1 METHODS
+
+=head2 call( $env )
+
+=head1 ENV VALUES
+
+After successful verifying authorization within middleware layer,
+Following 3 type of values are set in env.
+
+=over 4
+
+=item REMOTE_USER
+
+Identifier of user who grant the client to access the user's protected
+resource that is stored on service provider.
+
+=item X_OAUTH_CLIENT
+
+Identifier of the client that accesses to user's protected resource
+on beharf of the user.
+
+=item X_OAUTH_SCOPE
+
+Scope parameter that represents what kind of resources that
+the user grant client to access.
+
+=back
+
+=head1 AUTHOR
+
+Lyo Kato, E<lt>lyo.kato@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Lyo Kato
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
 
 1;
