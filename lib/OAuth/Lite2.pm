3d7c684ea84d86988387d47898f93fab99040479
lyokato <lyo.kato@gmail.com>
2011-11-07 19:51:53 +0900

    removed SSL warning

--- lib/OAuth/Lite2.pm
+++ lib/OAuth/Lite2.pm
@@ -3,7 +3,7 @@ package OAuth::Lite2;
 use strict;
 use warnings;
 
-our $VERSION = '0.01_13';
+our $VERSION = '0.01_14';
 
 1;
 __END__
--- lib/Plack/Middleware/Auth/OAuth2/ProtectedResource.pm
+++ lib/Plack/Middleware/Auth/OAuth2/ProtectedResource.pm
@@ -22,7 +22,7 @@ sub call {
 
         # after draft-v6, signature is not required, so always each connection
         # should be under TLS.
-        warn "insecure bearere token request" unless $req->secure;
+        # warn "insecure bearere token request" unless $req->secure;
 
         my $parser = OAuth::Lite2::ParamMethods->get_param_parser($req)
             or OAuth::Lite2::Server::Error::InvalidRequest->throw;
